4eac2ecb0d97d988d2e2c8cc58f207f5dfb5ad07
Nadine Estrella Vacas <nadinestrella@gmail.com>
2024-03-08 10:34:03 +0100

    chapter 3

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+//traemos el css al global 
+import '@/app/ui/global.css';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
